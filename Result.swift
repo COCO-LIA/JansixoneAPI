fdef224b042e91e38a83f94ab4b943beef89335d
LIA <lvxing29@naver.com>
2021-01-09 17:15:13 +0900

    decoding, 네트워킹, 파싱

--- Result.swift
+++ Result.swift
@@ -7,10 +7,58 @@
 
 import Foundation
 
-struct Result {
+//데이터파싱 2-2. 주석처리
+//struct Result {
+//    var language : String = ""
+//    var title : String = ""
+//    var overview : String = ""
+//    //var popularity : Float = 0
+//    var video : Bool = false
+//}
+
+//데이터파싱 2-3 codable 형식. 찍어내듯이 담아서 데이터를 넣어주는 방법
+//초기값을 줄 때는 let이 아니라 var로 선언한다. let은 상수라서 다음번에 정보를 가진 JSON이 들어오더라도 고정되기 떄문
+
+struct Result: Codable {
     var language : String = ""
     var title : String = ""
-    var overview : String = ""
-    //var popularity : Float = 0
+    var overView : String = ""
+    var popularity : Float = 0.0
     var video : Bool = false
+    var poster : String = ""
+    
+                   //형식
+    enum CodingKeys: String, CodingKey {
+        case language = "original_language"
+        case title = "original_title"
+        case overView = "overview"
+        case popularity
+        case video
+        case poster = "poster_path"
+        //하위 요소일경우 case country(하위) = "location"(상위)식으로 작성
+    }
+    
+    //enum LocationKeys : String, CodingKey {
+    //  case country }
+    
+    //decoder로부터 초기화를 설정하는 것
+    //Decode:JSON 데이터를 decodable 자료형에 저장하는 작업이다. 마찬가지로 try의 사용을 강제하고, 원본 데이터로 Data 자료형을 요구한다.
+    init(from decoder:Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        language = try values.decode(String.self, forKey: .language)
+        title = try values.decode(String.self, forKey: .title)
+        overView = try values.decode(String.self, forKey: .overView)
+        //하위 요소일 경우
+        //let locatio = try values.mestedContainer(KeyedBy: LocationKeys.self, forKey: .country)
+        //country = try location.decode(String.self, forKey: .country)
+        popularity = try values.decode(Float.self, forKey: .popularity)
+        video = try values.decode(Bool.self, forKey: .video)
+        poster = try values.decode(String.self, forKey: .poster)
+    }
 }
+
+struct ResultDataStore: Codable {
+    var results: [Result]
+}
+
+//2-4 viewController파일로 가기
--- ViewController.swift
+++ ViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import ProgressHUD
+//ALAMOFIRE 1-3
+import Alamofire
+import Kingfisher
 
 class ViewController: UIViewController {
     
@@ -27,51 +31,119 @@ class ViewController: UIViewController {
         let resultUrl = URL(string: originalAddress)
         let request = URLRequest(url: resultUrl!)
         
-        //네트워킹 //session 접속하기 / task 특정작업 수행하기
-        URLSession.shared.dataTask(with: request) { [self] (data, response, err) in
-            print("+++++++res++++++++", response)
-            if err != nil {
-                return print("err", err?.localizedDescription)
-            } else {
-                self.results = parseJsonData(orgData: data!)
-                
-                //*테이블뷰 새로고침
-                OperationQueue.main.addOperation {
-                    self.tableView.reloadData()
-                }
+        ProgressHUD.animationType = .lineScaling
+        //ProgressHUD.showProgress(1.5)
+        ProgressHUD.show()
+        
+        //네트워킹 방법 3 Json 형태
+        
+        AF.request(request).responseJSON { (data) in
+            if data.error != nil {
+                ProgressHUD.dismiss()
+                return print("err", data.error?.localizedDescription as Any)
             }
             
-        }.resume()
+            print(data.data)
+            self.results = self.parseJsonData(orgData: data.data!)
+            OperationQueue.main.addOperation {
+                self.tableView.reloadData()
+                ProgressHUD.dismiss()
+            }
+//            print("+++++++++++", self.results)
+            
+        }
+        
+        
+        //네트워킹 방법2 ALAMOFIRE
+        //ALAMOFIRE 1-1 .pods > podfile > pod 'Alamofire' 추가
+        //ALAMOFIRE 1-2. 추가 후 터미널 > pod install
+        //ALAMOFIRE 1-4. (Making request)
+        
+         //af에 요청 하고     -  요청 응답을 데이터 형식으로 받은것
+//        AF.request(request).responseData { (data) in
+//           // print(data.data) data.err
+//            if data.error != nil {
+//                ProgressHUD.dismiss()
+//                return print("err", data.error?.localizedDescription)
+//            } else { //데이터파싱
+//                //오리지널 데이터 = data.data
+//                self.results = self.parseJsonData(orgData: data.data!)
+//                OperationQueue.main.addOperation {
+//                    self.tableView.reloadData()
+//                    ProgressHUD.dismiss()
+//                }
+//            }
+//        }.resume()
+//    }
+        
+       
+        
+//        //네트워킹 방법1. url session //session 접속하기 / task 특정작업 수행하기
+//        URLSession.shared.dataTask(with: request) { [self] (data, response, err) in
+//            print("+++++++res++++++++", response)
+//            if err != nil {
+//                ProgressHUD.dismiss()
+//                return print("err", err?.localizedDescription)
+//            } else {
+//                self.results = parseJsonData(orgData: data!)
+//
+//                //*테이블뷰 새로고침
+//                OperationQueue.main.addOperation {
+//                    self.tableView.reloadData()
+//                    ProgressHUD.dismiss()
+//                }
+//            }
+//
+//        }.resume()
         
     }
     //전체 데이터 json파싱
     func parseJsonData(orgData:Data) -> [Result] {
         self.results = [Result]()
         
-        do {
-            
-            //웹에 있는 자료
-            let jsonResult = try JSONSerialization.jsonObject(with: orgData, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
-            
-            let jsonResults = jsonResult["results"] as! [AnyObject]
-            print(jsonResults)
-            
-            for jsonResult in jsonResults {
-                var result = Result()
-                
-                result.language = jsonResult["original_language"] as! String
-                result.title = jsonResult["original_title"] as! String
-                result.overview = jsonResult["overview"] as! String
-             //   result.popularity = jsonResult["popularity"] as! Float
-                result.video = jsonResult["video"] as! Bool
-                
-                results.append(result)
-            }
-            
+        //데이터파싱 2-4.
+        let decoder = JSONDecoder()
+        
+        do {                                     //decoding할것을 넣어줌
+      //Decode:JSON 데이터를 decodable 자료형에 저장하는 작업이다. 마찬가지로 try의 사용을 강제하고, 원본 데이터로 Data 자료형을 요구한다.
+         let resultDataStore = try decoder.decode(ResultDataStore.self, from: orgData)
+            print("___________-______", resultDataStore.results)
+            results = resultDataStore.results
         } catch {
-            print("----------------",error)
+            print(error)
         }
+        
         return results
+        
+        
+        //데이터 파싱방법 1
+//        do {
+//
+//            //웹에 있는 자료
+//            let jsonResult = try JSONSerialization.jsonObject(with: orgData, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String : AnyObject]
+//
+//            let jsonResults = jsonResult["results"] as! [AnyObject]
+//            print(jsonResults)
+//
+//            for jsonResult in jsonResults {
+//                var result = Result()
+//
+//                result.language = jsonResult["original_language"] as! String
+//                result.title = jsonResult["original_title"] as! String
+//                result.overview = jsonResult["overview"] as! String
+//             //   result.popularity = jsonResult["popularity"] as! Float
+//                result.video = jsonResult["video"] as! Bool
+//
+//                results.append(result)
+//            }
+//
+//        } catch {
+//            print("----------------",error)
+//        }
+        
+        //데이터 파싱방법 2.
+        //2-1.Result로 간다
+//        return results
     }
     
 }
@@ -92,8 +164,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         cell1.languageLabel.text = results[indexPath.row].language
         cell1.titleLabel.text = results[indexPath.row].title
         cell1.videoLabel.text = "\(results[indexPath.row].video)"
-        cell1.overViewLabel.text =
-            results[indexPath.row].overview
+        cell1.overViewLabel.text = results[indexPath.row].overView
+        let imgurl = URL(string: "https://image.tmdb.org/t/p/w500\(results[indexPath.row].poster)")
+        cell1.imgView.kf.setImage(with: imgurl)
         
         return cell1
     }
